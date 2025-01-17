53dad7f94bc8e1d055a8d29c588467fa5a936b8f
JonasMaia12 <jonaspiresmaia7@gmail.com>
2025-01-17 09:54:50 +0000

    Add Form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,9 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="grid-cols-app grid min-h-screen">
+        <div className="grid min-h-screen grid-cols-app">
           <Sidebar />
-          <main className="px-4 pb-12 pt-8">{children}</main>
+          <main className="px-8 pb-12 pt-8">{children}</main>
         </div>
       </body>
     </html>
